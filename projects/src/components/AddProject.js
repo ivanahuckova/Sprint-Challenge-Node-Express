8ab7f4f6ad56a069ba8978b8d193a2daddf65419
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-22 19:55:55 +0100

    Reuse add for for updating

--- projects/src/components/AddProject.js
+++ projects/src/components/AddProject.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default class AddProject extends React.Component {
   constructor(props) {
     super(props);
+
     this.nameRef = React.createRef();
     this.descriptionRef = React.createRef();
     this.completedRef = React.createRef();
@@ -15,38 +16,41 @@ export default class AddProject extends React.Component {
   };
 
   render() {
+    const updatingProject = this.props.currentProject;
     return (
       <div>
         <h3>
-          {this.props.currentProject && <div>Update the project</div>}
-          {!this.props.currentProject && <div>Add a New Project</div>}
+          {updatingProject && <div>Update the project</div>}
+          {!updatingProject && <div>Add a New Project</div>}
         </h3>
         <form>
           <div>
             Project Name:
-            <input type="text" ref={this.nameRef} />
+            <input type="text" ref={this.nameRef} defaultValue={(!updatingProject && '') || (updatingProject && updatingProject.name)} />
           </div>
           <div>
             Project Description:
-            <input type="text" ref={this.descriptionRef} />
+            <input type="text" ref={this.descriptionRef} defaultValue={(!updatingProject && '') || (updatingProject && updatingProject.description)} />
           </div>
           <div>
-            {' '}
             Completed:
             <input
               type="checkbox"
               ref={input => {
                 this.completedRef = input;
               }}
+              defaultChecked={(!updatingProject && false) || (updatingProject && updatingProject.completed)}
             />
           </div>
+
           <button
             onClick={e => {
               e.preventDefault();
-              this.props.axios_postProject(this.nameRef.current.value, this.descriptionRef.current.value, this.completedRef.checked);
+              (!updatingProject && this.props.axios_postProject(this.nameRef.current.value, this.descriptionRef.current.value, this.completedRef.checked)) ||
+                (updatingProject && this.props.axios_updateProject(updatingProject.id, this.nameRef.current.value, this.descriptionRef.current.value, this.completedRef.checked));
               this.clearInputs();
             }}>
-            Add project
+            {(!updatingProject && 'Add project') || (updatingProject && 'Update project')}
           </button>
         </form>
       </div>
