f1e687608e00b78d89d9cbedbfb0017cdfc006b9
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-22 20:29:02 +0100

    Refactor server

--- api/server.js
+++ api/server.js
@@ -5,17 +5,12 @@ const actionRoutes = require('./routes/actionRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const server = express();
 
-const pathToIndexHtml = path.join(__dirname, '../projects/build/index.html');
 const pathToBuildFolder = path.join(__dirname, '../projects/build');
 
 server.use(express.json());
 server.use(cors());
 
-server.use(express.static(pathToBuildFolder));
-server.get('/', (req, res) => {
-  res.sendFile(pathToIndexHtml);
-});
-
+server.use('/', express.static(pathToBuildFolder));
 server.use('/actions', actionRoutes);
 server.use('/projects', projectRoutes);
 
