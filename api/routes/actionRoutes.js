14325a1204f2f2bdd27bb519c83c9961eca2986b
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-22 09:49:48 +0100

    Setup of get actions routes

---
+++ api/routes/actionRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const actionDb = require('../../data/helpers/actionModel');
+
+const routes = express.Router();
+
+// =============== GET ROUTES =============== //
+
+routes.get('/', async (req, res) => {
+  try {
+    const allActions = await actionDb.get();
+    res.status(200).json(allActions);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+routes.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const specificAction = await actionDb.get(id);
+    console.log(specificAction);
+    if (specificAction) {
+      res.status(200).json(specificAction);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+module.exports = routes;
--- api/server.js
+++ api/server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const actionRoutes = require('./routes/actionRoutes');
-const projectRoutes = require('./routes/projectRoutes');
 
 const server = express();
 
 server.use(express.json());
-server.use(cors());
+// server.use(cors());
 
-// server.use('/actions', actionRoutes);
-// server.use('projects', projectRoutes);
+server.use('/actions', actionRoutes);
 
 module.exports = server;
