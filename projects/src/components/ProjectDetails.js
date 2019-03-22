23571204d8524c65e107e0406ffc2f74b9c6fddb
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-22 11:24:25 +0100

    Create fully working and styled Project Details Component

--- projects/src/components/ProjectDetails.js
+++ projects/src/components/ProjectDetails.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledProjectDetails = styled.div`
+  margin: 10px 0;
+  padding: 10px;
+  border: 0.5px solid green;
+
+  .project-details-row {
+    padding: 5px 0;
+
+    .project-details-span {
+      color: green;
+      font-weight: bold;
+    }
+  }
+`;
 
 export default function ProjectDetails(props) {
   return (
-    <div>
-      <div>
-        <span>Project Name:</span>
+    <StyledProjectDetails>
+      <div className="project-details-row">
+        <span className="project-details-span">Project Name:</span>
         {props.project.name}
       </div>
-    </div>
+      <div className="project-details-row">
+        <span className="project-details-span">Project Description:</span>
+        {props.project.description}
+      </div>
+      <div className="project-details-row">
+        <span className="project-details-span">Completed:</span>
+        {props.project.description && 'Yes'}
+        {!props.project.description && 'No'}
+      </div>
+    </StyledProjectDetails>
   );
 }
